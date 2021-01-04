43c07f77c0e15df68a1bc6f8bb19981cd33f7af4
es-rene99 <es.rene99@gmail.com>
2021-01-04 17:19:28 -0600

    refactor: getting stylesheet

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
-const sizeOfGrid = 225;
+const squaresPerSide = 16;
+const squareNumber = squaresPerSide ** 2;
+const sizeOfGrid = `${960}px`;
+
 const boardElement = document.querySelector('#board');
-for (let i = 0; i < sizeOfGrid; i++) {
+for (let i = 0; i < squareNumber; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   boardElement.appendChild(square);
 }
 
+function getStyleSheet(unique_title) {
+  for (let i = 0; i < document.styleSheets.length; i++) {
+    const sheet = document.styleSheets[i];
+    if (sheet.title === unique_title) {
+      return sheet;
+    }
+  }
+}
+// TODO left here, with this method I can insert css in my stylesheet like w3 mentions, instead of inline, need to alter it and be able to enter the size of the grid depending on the square per size
+
 const sheet = document.createElement('style');
 document.body.appendChild(sheet);
-sheet.innerHTML = '.square {border: 2px solid black;}';
+sheet.insertRule();
