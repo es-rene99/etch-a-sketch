f0ab07711bd40704fc33d0f65e18e5e77a4c9cb9
es-rene99 <es.rene99@gmail.com>
2021-01-04 13:17:08 -0600

    feat: able to manipulate css dynamically

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-const sizeOfGrid = 256;
+const sizeOfGrid = 225;
 const boardElement = document.querySelector('#board');
 for (let i = 0; i < sizeOfGrid; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   boardElement.appendChild(square);
 }
+
+const sheet = document.createElement('style');
+document.body.appendChild(sheet);
+sheet.innerHTML = '.square {border: 2px solid black;}';
