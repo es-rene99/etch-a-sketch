7c862ef320b0ab29561b0e0c916a498ace4d2903
es-rene99 <es.rene99@gmail.com>
2021-01-06 17:58:22 -0600

    feat: added function setGridNewSize

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
-const squaresPerSide = 15;
-const squareNumber = squaresPerSide ** 2;
+let squaresPerSide = 15;
+let squareNumber;
+function getSquareNumber() {
+  squareNumber = squaresPerSide ** 2;
+}
+getSquareNumber();
 const sizeOfGrid = `${460}px`;
 
 document.documentElement.style.setProperty('--sizeOfGrid', `${sizeOfGrid}`);
@@ -28,3 +32,31 @@ resetGrid.addEventListener('click', () => {
     square.classList.remove('colorized-square');
   });
 });
+
+function isValidNumber(paramToEvaluate) {
+  return paramToEvaluate > 0
+    && paramToEvaluate !== undefined
+    && paramToEvaluate !== null
+    && !(Number.isNaN(paramToEvaluate));
+}
+
+function setGridNewSize() {
+  do {
+    if (!(isValidNumber(squaresPerSide))) {
+      alert('Please enter a valid number');
+    }
+    squaresPerSide = prompt('How many squares per side would you like?', 16);
+    // TODO how to cancel this if the user hits cancel
+  } while (!(isValidNumber(squaresPerSide)));
+}
+
+function readjustGrid() {
+
+}
+
+function changeGridSize() {
+  setGridNewSize();
+  readjustGrid();
+}
+const change = document.querySelector('#change-size');
+change.addEventListener('click', changeGridSize);
