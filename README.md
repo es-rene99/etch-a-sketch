ad09de8c401b94774699cdcc83382b7ffd78730d
es-rene99 <es.rene99@gmail.com>
2021-01-08 13:28:21 -0600

    feat: implementing error handling

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Cool things I learned
 
 - I tried using canvas but apparently it requires different js methods [https://www.w3schools.com/html/html5_canvas.asp]
+- There's a lot of discussion for the best way to remove multiple children of an HTML but the best way appears to be setting the node.innerHTML to empty based on Gabriel McAdams and npjohns answer. [https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript]
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,49 @@
-let squaresPerSide = 15;
+const defaultSquarePerSide = 16;
 let squareNumber;
-function getSquareNumber() {
-  squareNumber = squaresPerSide ** 2;
-}
-getSquareNumber();
+let gridSquares;
 const sizeOfGrid = `${460}px`;
-
-document.documentElement.style.setProperty('--sizeOfGrid', `${sizeOfGrid}`);
-document.documentElement.style.setProperty('--squaresPerSide', `${squaresPerSide}`);
-
 const boardElement = document.querySelector('#board');
-for (let i = 0; i < squareNumber; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  boardElement.appendChild(square);
+const squarePerSideElement = document.querySelector('#squarePerSize');
+const change = document.querySelector('#change-size');
+
+const resetGrid = document.querySelector('#reset-grid');
+document.documentElement.style.setProperty('--sizeOfGrid', `${sizeOfGrid}`);
+
+const errorMessage = {
+  NotValidNumber: '* The value must be a whole positive number',
+  outOfRange: '* The number must be between 0 and 100',
+};
+
+// TODO left here assign error message
+
+function setSquaresPerSideCSS(squarePerSide) {
+  document.documentElement.style.setProperty('--squaresPerSide', `${squarePerSide}`);
 }
 
-const gridSquares = document.querySelectorAll('.square');
+function setSquarePerNumber(squarePerSide) {
+  squareNumber = squarePerSide ** 2;
+  setSquaresPerSideCSS(squarePerSide);
+}
 
 function setColorizedSquare() {
   this.classList.add('colorized-square');
 }
 
-gridSquares.forEach((square) => {
-  square.addEventListener('mouseover', setColorizedSquare);
-});
+function removeBoard() {
+  boardElement.innerHTML = '';
+}
 
-const resetGrid = document.querySelector('#reset-grid');
-resetGrid.addEventListener('click', () => {
+function createBoard() {
+  for (let i = 0; i < squareNumber; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    boardElement.appendChild(square);
+  }
+  gridSquares = document.querySelectorAll('.square');
   gridSquares.forEach((square) => {
-    square.classList.remove('colorized-square');
+    square.addEventListener('mouseover', setColorizedSquare);
   });
-});
+}
 
 function isValidNumber(paramToEvaluate) {
   return paramToEvaluate > 0
@@ -40,23 +52,35 @@ function isValidNumber(paramToEvaluate) {
     && !(Number.isNaN(paramToEvaluate));
 }
 
-function setGridNewSize() {
-  do {
-    if (!(isValidNumber(squaresPerSide))) {
-      alert('Please enter a valid number');
-    }
-    squaresPerSide = prompt('How many squares per side would you like?', 16);
-    // TODO how to cancel this if the user hits cancel
-  } while (!(isValidNumber(squaresPerSide)));
+function setGridNewSize(gridNewValue) {
+  if (!(isValidNumber(gridNewValue))) {
+    return;
+  }
+  setSquarePerNumber(gridNewValue);
 }
 
 function readjustGrid() {
-
+  removeBoard();
+  createBoard();
+  // TODO why is hover not working?
 }
 
 function changeGridSize() {
-  setGridNewSize();
+  setGridNewSize(+squarePerSideElement.value);
   readjustGrid();
 }
-const change = document.querySelector('#change-size');
+
 change.addEventListener('click', changeGridSize);
+
+resetGrid.addEventListener('click', () => {
+  gridSquares.forEach((square) => {
+    square.classList.remove('colorized-square');
+  });
+});
+
+setSquarePerNumber(defaultSquarePerSide);
+createBoard();
+
+// TODO add an exception handling for Not a number exception else if 0 to 100 value exception else pass!
+
+// TODO when the user selects square 0 display a message showing you selected 0 value, and well that means no squares... what did you thought was going to happen?
